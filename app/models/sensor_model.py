c97d31502237dc1cb4bf156c62f1686292298874
Augustin Bernachon Irisarri <abernachon@gmail.com>
2016-08-29 17:44:08 +0200

    Update model sensor : Fixed uniqueConstraints and __table_args__

--- app/models/sensor_model.py
+++ app/models/sensor_model.py
@@ -4,10 +4,10 @@ from app import db
 class SensorModel(db.Model):
     __tablename__ = 'sensors'
     id = db.Column(db.Integer, primary_key=True)
-    sensor_type_id = db.Column(db.Integer, db.ForeignKey('sensor_type.id'))
-    name = db.Column(db.String(42), unique=True)
+    sensor_type_id = db.Column(db.Integer, db.ForeignKey('sensor_type.id'), nullable=False)
+    name = db.Column(db.String(42), nullable=False)
     value = db.Column(db.Integer)
-    __table_args__ = (db.UniqueConstraint(sensor_type_id, name, name='uniq_name_type'),)
+    __table_args__ = (db.UniqueConstraint('sensor_type_id', 'name', name='uniq_name_type'),)
 
     def __init__(self, sensor_name, sensor_type_id, sensor_value=None):
         self.sensor_type_id = sensor_type_id
