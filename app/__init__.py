1a323bb2a88971991e39f542237780f5407a2229
Augustin Bernachon Irisarri <abernachon@gmail.com>
2016-10-12 21:16:09 +0200

    Update api routes : removed list all sensor of a given type from GET api/sensors

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_PATH
 db = SQLAlchemy(app)
 from app.resources import (Sensor, SensorType)
 
-api.add_resource(Sensor, '/api/sensors/<int:sensor_id>/', '/api/sensors/<int:sensor_type_id>/', '/api/sensors/')
+api.add_resource(Sensor, '/api/sensors/<int:sensor_id>/', '/api/sensors/')
 api.add_resource(SensorType, '/api/sensortypes/<int:type_id>', '/api/sensortypes/')
 
 """
