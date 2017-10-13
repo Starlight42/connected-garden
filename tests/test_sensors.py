7c4d3824ebce97ac0975885097aaf9cef17faaf7
Augustin Bernachon Irisarri <abernachon@gmail.com>
2017-10-14 01:07:11 +0200

    Added basic testing folder and file

---
+++ tests/test_sensors.py
@@ -0,0 +1,49 @@
+class TestSensors:
+    def test_get_one_sensor(self):
+        pass
+
+    def test_get_all_sensors(self):
+        pass
+
+    def test_get_unexisting_sensor(self):
+        pass
+
+    def test_post_new_sensor(self):
+        pass
+
+    def test_post_existing_sensor(self):
+        pass
+
+    def test_post_wrong_param(self):
+        pass
+
+    def test_post_no_param(self):
+        pass
+
+    def test_post_not_json_param(self):
+        pass
+
+    def test_patch_sensor(self):
+        pass
+
+    def test_patch_unexisting_sensor(self):
+        pass
+
+    def test_patch_wrong_param(self):
+        pass
+
+    def test_patch_no_param(self):
+        pass
+
+    def test_patch_not_json_param(self):
+        pass
+
+    def test_patch_unauthorized(self):
+        pass
+
+    def test_delete_sensor(self):
+        pass
+
+    def test_delete_unexisting_sensor(self):
+        pass
+
